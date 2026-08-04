eee4fd5f19610025158c296b63beb234e00e1fdc
cyberkdh <cyberkdh@gmail.com>
2026-08-04 09:40:15 +0900

    strong name sign 적용

--- DHWifiClientSol/lib/DHWifiClient.NET/Properties/AssemblyInfo.cs
+++ DHWifiClientSol/lib/DHWifiClient.NET/Properties/AssemblyInfo.cs
@@ -2,7 +2,7 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: InternalsVisibleTo("DHWifiClient.NET.Tests")]
+[assembly: InternalsVisibleTo("DHWifiClient.NET.Tests, PublicKey=0024000004800000940000000602000000240000525341310004000001000100fd548a2004f335e8e0c844bdef4b6b24a2b5dfe7e64be03b6d81b8969b42c0addda5e80d1eb13e420925f1ba56102f8dcfe017be8eeb064ddb5b8fbe62342f10aad55bfdac8f45d21f06b4103bce05eff6c714b94f95fc2cf176c64d8514100a64f64e56ccf87fefd49efea2a8268c8c0cc3f1d2094b8d5b8ff6de44f0b529e1")]
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
